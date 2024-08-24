0aa512547adcda9138cff59c1608c73c09989980
pedroggwp <phsnnascimento@outlook.com>
2024-08-24 14:35:14 -0300

    resolving conflits

--- app/src/main/java/com/ottistech/indespensa/shared/AppConstants.kt
+++ app/src/main/java/com/ottistech/indespensa/shared/AppConstants.kt
@@ -6,4 +6,12 @@ object AppConstants {
         "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI",
         "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"
     )
+
+    val START_CAROUSEL_MESSAGES : List<String> = listOf(
+        "Organize sua despensa pessoal",
+        "Gerenciamento de estoque para pequenas empresas",
+        "Indicação de receitas para todos os perfis",
+        "Otimize seu tempo e esforço na cozinha",
+        "Economize dinheiro sem desperdício"
+    )
 }
