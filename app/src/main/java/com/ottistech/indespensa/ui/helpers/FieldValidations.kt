e7137ffda00b65c5e13aec51527ce5e156cf3dbc
pedroggwp <phsnnascimento@outlook.com>
2024-08-24 14:01:52 -0300

    removing method validIsEmailDetailed cause we are already doing it

--- app/src/main/java/com/ottistech/indespensa/ui/helpers/FieldValidations.kt
+++ app/src/main/java/com/ottistech/indespensa/ui/helpers/FieldValidations.kt
@@ -1,7 +1,6 @@
 package com.ottistech.indespensa.ui.helpers
 
 import android.content.Context
-import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.widget.CheckBox
