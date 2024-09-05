aa56f6f6cdcec7fed6f8d570c81978a82a955fc5
pedroggwp <phsnnascimento@outlook.com>
2024-09-05 11:36:23 -0300

    deactivate user methods

--- app/src/main/java/com/ottistech/indespensa/data/repository/UserRepository.kt
+++ app/src/main/java/com/ottistech/indespensa/data/repository/UserRepository.kt
@@ -6,6 +6,7 @@ import com.ottistech.indespensa.data.datasource.UserFirebaseDataSource
 import com.ottistech.indespensa.data.datasource.UserRemoteDataSource
 import com.ottistech.indespensa.data.exception.BadRequestException
 import com.ottistech.indespensa.data.exception.FieldConflictException
+import com.ottistech.indespensa.data.exception.ResourceGoneException
 import com.ottistech.indespensa.data.exception.ResourceNotFoundException
 import com.ottistech.indespensa.data.exception.ResourceUnauthorizedException
 import com.ottistech.indespensa.webclient.dto.UserCreateDTO
@@ -53,23 +54,28 @@ class UserRepository (
                     else -> false
                 }
             }
-            else -> false
+            else -> {
+                Log.e(TAG, "[signupUser] Unexpected result: $result")
+                false
+            }
         }
     }
 
     suspend fun loginUser(user: UserLoginDTO) : Boolean {
         val result : ResultWrapper<UserCredentialsDTO> = remoteDataSource.loginUser(user)
+
         return when(result) {
             is ResultWrapper.Success -> {
-                Log.d(TAG, "[getUser] Logging user successfully: ${result.value}")
+                Log.d(TAG, "[loginUser] Logging user successfully: ${result.value}")
                 firebaseDataSource.loginUser(
                     email = user.email,
                     password = user.password
                 )
                 true
             }
-
             is ResultWrapper.Error -> {
+                Log.e(TAG, "[loginUser] Error while logging user: ${result.error}, code: ${result.code}")
+
                 when(result.code) {
                     HttpURLConnection.HTTP_NOT_FOUND -> {
                         throw ResourceNotFoundException(result.error)
@@ -77,22 +83,28 @@ class UserRepository (
                     HttpURLConnection.HTTP_UNAUTHORIZED -> {
                         throw ResourceUnauthorizedException(result.error)
                     }
+                    HttpURLConnection.HTTP_GONE -> {
+                        throw ResourceGoneException(result.error)
+                    }
 
                     else -> false
                 }
             }
-            else -> false
+            else -> {
+                Log.e(TAG, "[loginUser] Unexpected result: $result")
+                false
+            }
         }
     }
 
     suspend fun infoUserDetails(userId: Long, fullInfo: Boolean) : UserFullCredentialsDTO? {
         val result: ResultWrapper<UserFullCredentialsDTO> = remoteDataSource.getUserFullInfo(userId, fullInfo)
+
         return when (result) {
             is ResultWrapper.Success -> {
                 Log.d(TAG, "[infoUserDetails] Successfully retrieved user info: ${result.value}")
                 result.value
             }
-
             is ResultWrapper.Error -> {
                 Log.e(TAG, "[infoUserDetails] Error retrieving user info: ${result.error}, code: ${result.code}")
                 when (result.code) {
@@ -105,7 +117,6 @@ class UserRepository (
                     else -> null
                 }
             }
-
             else -> {
                 Log.e(TAG, "[infoUserDetails] Unexpected result: $result")
                 null
@@ -115,13 +126,12 @@ class UserRepository (
 
     suspend fun updateUser(userId: Long, user: UserUpdateDTO) : UserUpdateResponseDTO? {
         val result: ResultWrapper<UserUpdateResponseDTO> = remoteDataSource.updateUser(userId, user)
-        return when(result) {
 
+        return when(result) {
             is ResultWrapper.Success -> {
                 Log.d(TAG, "[updateUser] Successfully retrieved user info: ${result.value}")
                 result.value
             }
-
             is ResultWrapper.Error -> {
                 Log.e(TAG, "[updateUser] Error retrieving user info: ${result.error}, code: ${result.code}")
                 when (result.code) {
@@ -137,7 +147,6 @@ class UserRepository (
                     else -> null
                 }
             }
-
             else -> {
                 Log.e(TAG, "[updateUser] Unexpected result: $result")
                 null
@@ -145,5 +154,38 @@ class UserRepository (
         }
     }
 
+    fun logoutUser() {
+        firebaseDataSource.logoutUser()
+    }
+
+    suspend fun deactivateUser(userId: Long) : Boolean {
+        val result: ResultWrapper<Any> = remoteDataSource.deactivateUser(userId)
+
+        return when (result) {
+            is ResultWrapper.Success -> {
+                Log.d(TAG, "[deactivateUser] User deactivated successfully")
+                true
+            }
+            is ResultWrapper.Error -> {
+                Log.e(TAG, "[deactivateUser] Error deactivating user: ${result.error}, code: ${result.code}")
+                when (result.code) {
+                    HttpURLConnection.HTTP_NOT_FOUND -> {
+                        throw ResourceNotFoundException(result.error)
+                    }
+                    HttpURLConnection.HTTP_UNAUTHORIZED -> {
+                        throw ResourceUnauthorizedException(result.error)
+                    }
+                    HttpURLConnection.HTTP_BAD_REQUEST -> {
+                        throw BadRequestException(result.error)
+                    }
+                    else -> false
+                }
+            }
+            else -> {
+                Log.e(TAG, "[deactivateUser] Unexpected result: $result")
+                false
+            }
+        }
+    }
 
 }
