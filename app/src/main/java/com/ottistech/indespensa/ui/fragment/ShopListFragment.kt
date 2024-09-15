7d921afc860258f616dac5c17ffb22cb8cb88a9a
pedroggwp <phsnnascimento@outlook.com>
2024-09-14 23:11:20 -0300

    using api

--- app/src/main/java/com/ottistech/indespensa/ui/fragment/ShopListFragment.kt
+++ app/src/main/java/com/ottistech/indespensa/ui/fragment/ShopListFragment.kt
@@ -1,31 +1,42 @@
 package com.ottistech.indespensa.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.ottistech.indespensa.R
+import com.ottistech.indespensa.data.repository.ShopListRepository
+import com.ottistech.indespensa.data.repository.UserRepository
 import com.ottistech.indespensa.databinding.FragmentShoplistBinding
 import com.ottistech.indespensa.ui.recyclerview.adapter.ShopListAdapter
-import com.ottistech.indespensa.webclient.dto.ShopListIngredientResponseDTO
+import kotlinx.coroutines.launch
 
 class ShopListFragment : Fragment() {
 
-    private lateinit var binding : FragmentShoplistBinding
+    private lateinit var binding: FragmentShoplistBinding
     private lateinit var shopListAdapter: ShopListAdapter
+    private lateinit var shopListRepository: ShopListRepository
+    private lateinit var userRepository: UserRepository
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentShoplistBinding.inflate(inflater, container, false)
-
+        shopListRepository = ShopListRepository()
+        userRepository = UserRepository(requireContext())
         setupRecyclerView()
 
-        loadData()
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadData()
     }
 
     private fun setupRecyclerView() {
@@ -38,100 +49,15 @@ class ShopListFragment : Fragment() {
     }
 
     private fun loadData() {
-        val mockData = listOf(
-            ShopListIngredientResponseDTO(
-                listItemId = 31,
-                userId = 11,
-                productName = "Arroz",
-                imageUrl = "https://example.com/arroz.jpg",
-                amount = 12,
-                productAmount = 1.00,
-                productUnit = "kg"
-            ),
-            ShopListIngredientResponseDTO(
-                listItemId = 34,
-                userId = 11,
-                productName = "Feijão",
-                imageUrl = "https://example.com/feijao.jpg",
-                amount = 10,
-                productAmount = 1.00,
-                productUnit = "kg"
-            ),
-            ShopListIngredientResponseDTO(
-                listItemId = 34,
-                userId = 11,
-                productName = "Feijão",
-                imageUrl = "https://example.com/feijao.jpg",
-                amount = 10,
-                productAmount = 1.00,
-                productUnit = "kg"
-            ),
-            ShopListIngredientResponseDTO(
-                listItemId = 34,
-                userId = 11,
-                productName = "Feijão",
-                imageUrl = "https://example.com/feijao.jpg",
-                amount = 10,
-                productAmount = 1.00,
-                productUnit = "kg"
-            ),
-            ShopListIngredientResponseDTO(
-                listItemId = 34,
-                userId = 11,
-                productName = "Feijão",
-                imageUrl = "https://example.com/feijao.jpg",
-                amount = 10,
-                productAmount = 1.00,
-                productUnit = "kg"
-            ),
-            ShopListIngredientResponseDTO(
-                listItemId = 34,
-                userId = 11,
-                productName = "Feijão",
-                imageUrl = "https://example.com/feijao.jpg",
-                amount = 10,
-                productAmount = 1.00,
-                productUnit = "kg"
-            ),
-            ShopListIngredientResponseDTO(
-                listItemId = 34,
-                userId = 11,
-                productName = "Feijão",
-                imageUrl = "https://example.com/feijao.jpg",
-                amount = 10,
-                productAmount = 1.00,
-                productUnit = "kg"
-            ),
-            ShopListIngredientResponseDTO(
-                listItemId = 34,
-                userId = 11,
-                productName = "Feijão",
-                imageUrl = "https://example.com/feijao.jpg",
-                amount = 10,
-                productAmount = 1.00,
-                productUnit = "kg"
-            ),
-            ShopListIngredientResponseDTO(
-                listItemId = 34,
-                userId = 11,
-                productName = "Feijão",
-                imageUrl = "https://example.com/feijao.jpg",
-                amount = 10,
-                productAmount = 1.00,
-                productUnit = "kg"
-            ),
-            ShopListIngredientResponseDTO(
-                listItemId = 34,
-                userId = 11,
-                productName = "Feijão",
-                imageUrl = "https://example.com/feijao.jpg",
-                amount = 10,
-                productAmount = 1.00,
-                productUnit = "kg"
-            )
-        )
+        lifecycleScope.launch {
+            val currentUser = userRepository.getUserCredentials()
+            val shopItems = shopListRepository.listShopItems(currentUser.userId)
 
-        shopListAdapter = ShopListAdapter(mockData)
-        binding.shoplistRecyclerview.adapter = shopListAdapter
+            binding.shoplistQuantityIngredients.text = getString(R.string.shoplist_ingredients_count, shopItems.size)
+
+            shopListAdapter = ShopListAdapter(shopItems)
+            binding.shoplistRecyclerview.adapter = shopListAdapter
+        }
     }
 }
+
