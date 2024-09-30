a2eaebabd75f5aaf0c0896ada6b26dbefeee82b9
pedroggwp <phsnnascimento@outlook.com>
2024-09-30 08:34:39 -0300

    configuring premium fragment and premium dialog

--- app/src/main/java/com/ottistech/indespensa/ui/dialog/PaymentDialogCreator.kt
+++ app/src/main/java/com/ottistech/indespensa/ui/dialog/PaymentDialogCreator.kt
@@ -1,14 +1,19 @@
 package com.ottistech.indespensa.ui.dialog
 
 import android.app.AlertDialog
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.ottistech.indespensa.R
 import com.ottistech.indespensa.databinding.DialogPaymentBinding
+import com.ottistech.indespensa.shared.AppConstants.PIX_KEY
 import com.ottistech.indespensa.ui.helpers.makeSpanText
 
 class PaymentDialogCreator(
@@ -32,6 +37,10 @@ class PaymentDialogCreator(
              dialog.dismiss()
         }
 
+        binding.paymentConfirmCopyPix.setOnClickListener {
+            copyKeyPixClick()
+        }
+
         binding.premiumNormalText.text = makeSpanText(
             context.getString(R.string.payment_text, paymentAmount),
             paymentAmount,
@@ -45,4 +54,13 @@ class PaymentDialogCreator(
         val width = (context.resources.displayMetrics.widthPixels * 0.85).toInt()
         dialog.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
     }
+
+    private fun copyKeyPixClick() {
+        val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData = ClipData.newPlainText("Pix Key", PIX_KEY)
+        clipboardManager.setPrimaryClip(clipData)
+
+        Toast.makeText(context, "Chave pix copiada", Toast.LENGTH_SHORT).show()
+    }
+
 }
--- app/src/main/java/com/ottistech/indespensa/ui/fragment/PremiumFragment.kt
+++ app/src/main/java/com/ottistech/indespensa/ui/fragment/PremiumFragment.kt
@@ -66,6 +66,7 @@ class PremiumFragment : Fragment() {
                 )
             }
         }
+
     }
 
     override fun onDestroyView() {
